f8cb776fb0ea90dc3c3373339ec3f09e093d6238
Mahin2338 <mahinbusiness@hotmail.com>
2026-01-01 21:56:27 +0000

    fix terraform path

--- terraform/modules/outputs.tf
+++ terraform/modules/outputs.tf
@@ -1,7 +1,3 @@
-output "url_shortener_role_arn" {
-  value       = aws_iam_role.url_shortener.arn
-  description = "ARN of IAM role for URL shortener pods"
-}
 
 output "ecr_repository_url" {
   value = aws_ecr_repository.url-app.repository_url
