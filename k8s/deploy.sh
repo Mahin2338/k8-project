02623e5ce1c4fe5c1471ec1babb3779ed14bc2fd
Mahin2338 <mahinbusiness@hotmail.com>
2026-01-12 14:19:30 +0000

    fix script

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -3,6 +3,9 @@ set -e
 
 echo "Deploying app to eks"
 
+aws eks update-kubeconfig --name production-eks --region eu-west-2
+
+
 kubectl apply -f clusterissuer.yml
 kubectl apply -f deployment.yml
 kubectl apply -f service.yml
